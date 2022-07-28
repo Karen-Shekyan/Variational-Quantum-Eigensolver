de148a8b8535cf226186f9658b7f05aa2711235b
Code_Tomato <nathayoha@gmail.com>
2022-07-28 15:12:16 -0500

    Added filler unit tests

---
+++ qiskit code (Python)/UnitTest.py
@@ -0,0 +1,30 @@
+# Tests for Lab 12: Deutsch-Jozsa Algorithm
+# Copyright 2021 The MITRE Corporation. All Rights Reserved.
+
+import unittest
+
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute
+from qiskit import Aer
+import random
+
+
+class VQETests(unittest.TestCase):
+
+    def test1(self):
+
+        print("Test 1 passed!")
+
+
+    def test2(self):
+        
+        print("Test 2 passed!")
+
+
+    def test3(self):
+        
+        print("Test 3 passed!")
+
+
+if __name__ == '__main__':
+    unittest.main()
